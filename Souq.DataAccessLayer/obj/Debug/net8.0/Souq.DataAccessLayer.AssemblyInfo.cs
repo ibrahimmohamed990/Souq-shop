b7c3252e5fef089c43fb22fedeeaabf6663d85d4
ibrahimmohamed990 <ibrahimmohamed990>
2024-07-08 07:28:35 +0300

    update

--- Souq.DataAccessLayer/obj/Debug/net8.0/Souq.DataAccessLayer.AssemblyInfo.cs
+++ Souq.DataAccessLayer/obj/Debug/net8.0/Souq.DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Souq.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ee17e9e9ae8fee46705fcb939ec52873d468b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Souq.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Souq.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Souq.Web/obj/Debug/net8.0/Souq.Web.AssemblyInfo.cs
+++ Souq.Web/obj/Debug/net8.0/Souq.Web.AssemblyInfo.cs
@@ -12,10 +12,11 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d0dd1eff-4d5c-4969-9ff2-3a177f536994")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Souq.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ee17e9e9ae8fee46705fcb939ec52873d468b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Souq.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Souq.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
